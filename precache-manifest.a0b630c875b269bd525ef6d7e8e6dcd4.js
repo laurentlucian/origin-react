c8d80464989b19ff46030ea9baddad65c0ebaeab
Laurent <laurent.lucian@gmail.com>
2018-11-15 20:31:36 -0500

    Updates

---
+++ precache-manifest.a0b630c875b269bd525ef6d7e8e6dcd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e28cad9f477cf2211002",
+    "url": "/react-training/static/js/runtime~main.e28cad9f.js"
+  },
+  {
+    "revision": "24316eee3fdedc6d717b",
+    "url": "/react-training/static/js/main.24316eee.chunk.js"
+  },
+  {
+    "revision": "dbc9726dc28702fb5279",
+    "url": "/react-training/static/js/1.dbc9726d.chunk.js"
+  },
+  {
+    "revision": "24316eee3fdedc6d717b",
+    "url": "/react-training/static/css/main.a5385618.chunk.css"
+  },
+  {
+    "revision": "dbc9726dc28702fb5279",
+    "url": "/react-training/static/css/1.983f79db.chunk.css"
+  },
+  {
+    "revision": "4c5ebcbe08ab6430841a08ea85bec50b",
+    "url": "/react-training/index.html"
+  }
+];

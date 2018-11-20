ce1d378a2b470f3afc349b5ac8cf6db308cf6c56
Laurent <laurent.lucian@gmail.com>
2018-11-20 16:56:49 -0500

    changed from counting number to managing an array; added color change

--- my-app/src/components/Counter.jsx
+++ my-app/src/components/Counter.jsx
@@ -2,8 +2,38 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    bye: false
+    hello: [],
+    bye: false,
+    render: false
+  };
+
+  handleIncrement = () => {
+    const { hello } = this.state;
+    hello.push({ hello: "Hello", color: false });
+    this.setState({ hello, bye: false });
+  };
+
+  handleDelete = () => {
+    const hello = [];
+    this.setState({ hello, bye: true });
+  };
+
+  formatCount() {
+    const { hello } = this.state;
+    if (this.state.bye) {
+      return "Goodbye";
+    } else {
+      return hello.length === 0 ? "Hello!" : hello.length + " hey(s)";
+    }
+  }
+
+  boxColor = i => {
+    //fix this so it switches color between true or false
+    //replace only color and pushes in new array
+    const Hello = this.state.hello;
+    console.log(Hello[i]);
+    Hello[i].color = !Hello[i].color;
+    this.setState({ hello: Hello });
   };
 
   render() {
@@ -11,8 +41,15 @@ class Counter extends Component {
     for (let i = 0; i < this.state.count; i++) {
       hello.push("Hey");
     }
+
     return (
-      <div className="container box" style={{ width: 200, marginTop: 25 }}>
+      <div
+        className="container box"
+        style={{
+          maxWidth: 600,
+          marginTop: 25
+        }}
+      >
         <div
           style={{
             display: "flex",
@@ -24,14 +61,39 @@ class Counter extends Component {
           <button
             className="button is-primary box"
             onClick={this.handleIncrement}
+            onKeyDown={this.handleIncrement}
           >
-            {this.state.count === 0 ? "Say Hello" : "Hey Again"}
+            {this.state.hello.length === 0 ? "Say Hello" : "Again?"}
           </button>
-          {hello.map((hello, index) => (
-            <h1 key={index}>{hello}</h1>
-          ))}
-
-          {this.state.count >= 1 && (
+          <div
+            style={{
+              display: "flex",
+              maxWidth: 550,
+              flexWrap: "wrap",
+              justifyContent: "center",
+              maxHeight: 450,
+              overflow: "scroll"
+            }}
+          >
+            {this.state.hello.map((hello, index) => {
+              return (
+                <h1
+                  className={`box`} //changes class depending if variable is true or false
+                  style={{
+                    margin: "5px",
+                    backgroundColor: `${
+                      this.state.hello[index].color === true ? "red" : ""
+                    }`
+                  }}
+                  key={index}
+                  onClick={i => this.boxColor(index)}
+                >
+                  {hello.hello}
+                </h1>
+              );
+            })}
+          </div>
+          {this.state.hello.length !== 0 && (
             <div style={{ marginTop: 10 }}>
               <button
                 className="button is-primary box"
@@ -44,34 +106,6 @@ class Counter extends Component {
         </div>
       </div>
     );
-  }
-
-  handleIncrement = () => {
-    const count = this.state.count + 1;
-    this.setState({ count, bye: false });
-  };
-
-  handleDelete = () => {
-    const count = 0;
-    this.setState({ count, bye: true });
-  };
-
-  formatCount() {
-    const { count } = this.state;
-    if (this.state.bye) {
-      return "Goodbye";
-    } else {
-      return count === 0 ? "Hello!" : count + " hey(s)";
-    }
-  }
-
-  getClasses() {
-    let classes = "";
-    classes =
-      this.state.count === 0
-        ? "{backgroundColor: red}"
-        : "{backgroundColor: white}";
-    return classes;
   }
 }
 
